24880697ab76d5a8f53ea28ceba3f63ba4f8bdfb
Alessia Colpo <99196359+AllyJeager@users.noreply.github.com>
2023-05-26 14:28:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,33 @@
- // Ottieni i riferimenti agli elementi del DOM
-    const searchIcon = document.getElementById('search-icon');
-    const searchInput = document.querySelector('.search-input');
-   
-    // Aggiungi un gestore di eventi al clic sull'icona di ricerca
-    searchIcon.addEventListener('click', function() {
-      // Verifica se il campo di input di ricerca ha la classe 'show-search-input'
-      if (searchInput.classList.contains('show-search-input')) {
-        // Rimuovi la classe 'show-search-input' per nascondere il campo di input di ricerca
-        searchInput.classList.remove('show-search-input');
-      } else {
-        // Aggiungi la classe 'show-search-input' per mostrare il campo di input di ricerca
-        searchInput.classList.add('show-search-input');
-        searchInput.focus(); // Facoltativo: mette il focus sul campo di input
-      }
-    });
-   
- //Initialize Swiper 
-var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+// Ottieni i riferimenti agli elementi del DOM
+const searchIcon = document.getElementById('search-icon');
+const searchInput = document.querySelector('.search-input');
+
+// Aggiungi un gestore di eventi al clic sull'icona di ricerca
+searchIcon.addEventListener('click', function() {
+  // Verifica se il campo di input di ricerca ha la classe 'show-search-input'
+  if (searchInput.classList.contains('show-search-input')) {
+    // Rimuovi la classe 'show-search-input' per nascondere il campo di input di ricerca
+    searchInput.classList.remove('show-search-input');
+  } else {
+    // Aggiungi la classe 'show-search-input' per mostrare il campo di input di ricerca
+    searchInput.classList.add('show-search-input');
+    searchInput.focus(); // Facoltativo: mette il focus sul campo di input
+  }
 });
 
-//Initialize Swiper2
-     var swiper = new Swiper(".mySwiper2", {
-       slidesPerView: 1,
-       spaceBetween: 10,
-       pagination: {
-         el: ".swiper-pagination2",
-         clickable: true,
-       },
-       breakpoints: {
-         640: {
-           slidesPerView: 1,
-           spaceBetween: 20,
-         },
-         768: {
-           slidesPerView: 2,
-           spaceBetween: 40,
-         },
-         1024: {
-           slidesPerView: 3,
-           spaceBetween: 50,
-         },
-       },
-     });
-
+document.addEventListener('DOMContentLoaded', function() {
+  //Initialize Swiper
+  var swiper = new Swiper(".mySwiper1", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+});
  
