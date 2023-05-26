17a9d21936d83f028271efc537cbfa0734e36990
Alessia Colpo <99196359+AllyJeager@users.noreply.github.com>
2023-05-26 14:32:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -30,4 +30,35 @@ document.addEventListener('DOMContentLoaded', function() {
     },
   });
 });
+
+
+//Initialize Swiper2
+     var swiper = new Swiper(".mySwiper2", {
+       slidesPerView: 1,
+       spaceBetween: 10,
+       pagination: {
+         el: ".swiper-pagination2",
+         clickable: true,
+       },
+       breakpoints: {
+         640: {
+           slidesPerView: 1,
+           spaceBetween: 20,
+         },
+         768: {
+           slidesPerView: 2,
+           spaceBetween: 40,
+         },
+         1024: {
+           slidesPerView: 3,
+           spaceBetween: 50,
+         },
+       },
+     });
+
+//scrolltop
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
  
